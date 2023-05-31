587f50601b401d11b3b0714931b23556dae222cd
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-31 19:08:41 +0200

    added bonus script so that if you press the like it toggle color and also the count of likes and if profile img is missing it's gona get replaced with name and surname initials

--- main.js
+++ main.js
@@ -60,11 +60,14 @@ const container = document.querySelector('#container')
 const likesid = [];
 
 posts.forEach(post => {
+    let split = (post.created).split('-')
+    post.created = split[2]+'-'+split[1]+'-'+split[0]
+
     container.innerHTML += `<div class="post">
     <div class="post__header">
         <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+            <div class="post-meta__icon img${post.id}">
+                <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
             </div>
             <div class="post-meta__data">
                 <div class="post-meta__author">${post.author.name}</div>
@@ -90,16 +93,28 @@ posts.forEach(post => {
         </div> 
     </div>            
 </div>`;
+
 });
 
+
 for(let i=1; i<=posts.length; i++){
+const authorimg = document.querySelector('.img'+i)
 const like = document.getElementById('likes__cta'+i);
 const likecounterprint = document.getElementById('like-counter-'+i);
 const text = document.getElementById('like-button'+i);
+const name = (authorimg.firstElementChild.attributes.alt.textContent).replace(/[^A-Z]+/g, "");
+
+if((authorimg.firstElementChild.attributes[1].value) == "null"){
+    authorimg.innerHTML = `<p class="null-pic">${name}</p>`;
+}
 
 like.addEventListener('click', function(){
-    text.classList.add('color-red');
+    if(text.classList[2] === 'color-red'){
+        likecounterprint.innerHTML = parseInt(likecounterprint.innerHTML)-1;
+    }else{
     likecounterprint.innerHTML = parseInt(likecounterprint.innerHTML)+1;
+    }
+    text.classList.toggle('color-red');
     if(!likesid.includes(i)){
         likesid.push(i);
     }
